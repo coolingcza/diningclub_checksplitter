bc50ea9575126733addc5e9ebdaa36fc58f58713
Crystal Cooling <cooling.cne@gmail.com>
2015-01-29 13:05:05 -0600

    testing rm and some logic in classtest.rb

--- classtest.rb
+++ classtest.rb
@@ -4,13 +4,18 @@ class Event
   attr_reader :attendees
   attr_reader :destination
   
-  def initialize(attendees, destination)
+  def initialize(attendees, destination=nil)
     @attendees = attendees
     @destination = destination
   end
   
   def event_record
-    {destination: attendees}
+    if @destination
+      puts @destination
+    else
+      puts "nil"
+    end
+    #{destination: attendees} if destination
   end
   
 end
